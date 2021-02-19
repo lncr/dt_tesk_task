e971bb3ac9027ae88897984af003805e32cfe315
Bektur Soltobaev <roctivosu@gmail.com>
2021-02-19 13:53:07 +0600

    Update serializers to show comments

--- news/posts/serializers.py
+++ news/posts/serializers.py
@@ -3,10 +3,25 @@ from rest_framework import serializers
 from .models import Post, Comment
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "content", "creation_date"]
+        read_only_fields = [
+            "creation_date",
+        ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["author_name"] = instance.author.username
+        return representation
+
+
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name="post_detail_url", lookup_field="id"
     )
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
@@ -16,6 +31,7 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
             "url",
             "creation_date",
             "upvotes_amount",
+            "comments",
         ]
         read_only_fields = [
             "creation_date",
@@ -32,17 +48,3 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
         author = request.user
         new_post = Post.objects.create(author=author, title=validated_data["title"])
         return new_post
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ["id", "content", "creation_date"]
-        read_only_fields = [
-            "creation_date",
-        ]
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation["author_name"] = instance.author.username
-        return representation
